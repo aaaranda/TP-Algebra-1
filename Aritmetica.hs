89d5127ea490617c8055a0c68c25e15e09ef635b
aaaranda <33169914+aaaranda@users.noreply.github.com>
2017-11-05 13:03:35 -0300

    Add files via upload

--- Aritmetica.hs
+++ Aritmetica.hs
@@ -10,7 +10,9 @@ mcdExt _ _ = (0, (0, 0))
 
 --(2)
 criba :: Integer -> Set Integer
-criba _ = []
+criba x    | x == 1 = []
+           | esPrimo x = reverse (x : criba (x-1))
+           | otherwise = reverse(criba (x-1))
 
 --(3)
 coprimoCon:: Integer -> Integer
@@ -28,3 +30,18 @@ modExp :: Integer -> Integer -> Integer -> Integer
 modExp b 0 m = 1
 modExp b e m = t * modExp ((b * b) `mod` m) (shiftR e 1) m `mod` m
   where t = if testBit e 0 then b `mod` m else 1
+
+-- Funciones auxiliares para criba  
+
+esPrimo :: Integer -> Bool
+esPrimo x    	| (sumaDivisores x) == (x+1) = True
+                | otherwise = False
+ 
+sumaDivisoresHasta :: Integer -> Integer -> Integer
+sumaDivisoresHasta x y 		| y == 1 = 1
+                            | x `mod` y == 0 = y + sumaDivisoresHasta x (y-1)
+                            | otherwise = sumaDivisoresHasta x (y-1)
+ 
+sumaDivisores x = sumaDivisoresHasta x x
+
+-- Fin funciones auxiliares criba
